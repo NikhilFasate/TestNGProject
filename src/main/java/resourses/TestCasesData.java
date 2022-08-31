83163d4b5179de1ab67229a195bb5b250261d883
NikhilFasate <nikhilfasate9@gmail.com>
2022-08-31 21:31:29 +0530

    changes

--- src/main/java/resourses/TestCasesData.java
+++ src/main/java/resourses/TestCasesData.java
@@ -14,5 +14,5 @@ public class TestCasesData
 	public static String exceptedTitle="Free CRM Trial: Salesforce 30-Day Trial - Salesforce IN";
 	public static String exceptedString="Please check your username. If you still can't log in, contact your Salesforce administrator.";
 	public static String actualString="driver.findElement(By.xpath(\"//div[@id='error']\")).getText()";
-	
+	public static String actualTitle="driver.getTitle()";
 }
